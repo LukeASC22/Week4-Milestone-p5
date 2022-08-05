a5bfce44f834cb132a0fbd248298bfb4c443861a
Luke Burrell <ltburrell561@gmail.com>
2022-08-05 15:29:38 -0400

    week4 milsestone

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() { 
+    createCanvas(500,500); 
+    background(0,255,255); 
+    point(100,400); 
+    fill(255,200,0);
+    strokeWeight(2);
+    ellipse(273,285,300,300); 
+    fill(0,0,0);
+    strokeWeight(3);
+    ellipse(200,250,50,50); 
+    ellipse(350,250,50,50); 
+    line(200,350,350,350); 
+}
